f3a98a830de232a8e732110625e781d52c711c70
dr-winner <duvorrichardwinner@gmail.com>
2025-05-25 03:06:57 -0400

    complete module 1

--- modules/module1-fundamentals/exercises/01_hello_rust_starter.rs
+++ modules/module1-fundamentals/exercises/01_hello_rust_starter.rs
@@ -1,13 +1,25 @@
 use std::io;
+use chrono::Local;
 
 fn main() {
     // TODO: 1. Prompt the user for their name
+    println!("What is your name?");
+
 
     // TODO: 2. Read the user's input
     let mut name = String::new();
 
     // TODO: 3. Print a personalized greeting
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
 
     // BONUS: Print the current date
+
+    let current_date = Local::now().format("%Y-%m-%d").to_string();
+    println!("Hello, {}! Today's date is {}", name.trim(), current_date);
+
+
     // Hint: You can use the chrono crate for this
+    
 }
